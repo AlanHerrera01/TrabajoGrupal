309b52e5ef4cc5232de8b617de9cbb9adadb5653
AlanHerrera01 <avherrera3@espe.edu.ec>
2023-05-17 20:53:58 -0500

    Carperta Lista main

---
+++ Listas/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+typedef int Dato; 
+#include "Nodo.h"
+#include "Lista.h"
+using namespace std;
+int main()
+{
+Dato d;
+Lista lista; // crea lista vacía 
+  cout << "Elementos de la lista, termina con -1 "<< endl;
+  do {
+    cin>> d;
+    lista.insertarCabezaLista (d);
+  }while (d != -1); // recorre la lista para a escribir sus elementos
+  cout << "\t Elementos de la lista generados al azar" << endl;
+  lista.visualizar();
+  return 0;
+}
